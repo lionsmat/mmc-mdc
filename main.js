707b719c4a98f3795e5412daf42283b6e131d013
Leachenski <jcleachenski@gmail.com>
2024-06-19 10:13:34 -0300

    refactor: soma maior numero ao indice

--- main.js
+++ main.js
@@ -11,19 +11,24 @@ process.stdin.on("data", function(data) {
         numero2 = input
 
         let i
-
+        let maior
         if(numero1 > numero2) {
             i = numero1
+            maior = numero1
         } else {
             i = numero2
+            maior = numero2
         }
-
-        for (i; i <= numero1 * numero2; i++) {
+        
+        for (i; i <= numero1 * numero2; i += maior) {
             if(i % numero1 == 0 && i % numero2 == 0) {
                 console.log("MMC: " + i)
                 break
             }
         }
+
+        process.exit()
+
 
     }
 })
